24f8f11f3062a53fe08f2e4d7acfa4535955f111
Roman Cherepennikov <fpm.cherepen@bsu.by>
2021-03-21 13:18:22 +0300

    delete doGet method in CheckServlet

--- task5/java_servlets/src/java/com.bsu/CheckServlet.java
+++ task5/java_servlets/src/java/com.bsu/CheckServlet.java
@@ -9,14 +9,9 @@ import java.io.IOException;
 
 public class CheckServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         resp.setContentType("application/json");
         String response = "{\"sucess\":true}";
         resp.getWriter().println(response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        doGet(req,resp);
     }
 }
